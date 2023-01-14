2c92a4c4829ec53ebbea8a474eeaffee46aad1db
Danish NasirKhan Pathan <animestaan@gmail.com>
2023-01-14 09:14:01 +0530

    new

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,22 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import "./Testimonials.css";
+import { testimonialsData } from "../../data/testimonialsData";
+import leftArrow from '../../assets/leftArrow.png';
+import rightArrow from '../../assets/rightArrow.png'
 const Testimonials = () => {
+  const [selected,setSelected] = useState(0);
+  const tLength = testimonialsData.length
   return (
-    <div className='testimonials'>
+    <div className="testimonials">
       <div className="left-t">
-      <span>Testimonials</span>
-        <div className="why test-text">
+        <span>Testimonials</span>
+        <span className="stroke-text ">WHAT THEY</span>
+        <span>SAY ABOUT US</span>
+        <span>
+          {testimonialsData[selected].review}
+        </span>
+        <span>
+          <span style={{color:'var(--orange)'}}>
+            {testimonialsData[selected].name}
+          </span>{" "}-
+      
+            {testimonialsData[selected].status}
+          </span>
        
-          <span className="stroke-text ">WHAT THEY</span>
-          <span>SAY ABOUT US</span>
-        </div>
-        <span>I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!</span>
-        .
-        <span>MATHEW HANDRIKSON</span> 
-        <span>-ENTERPRENEUR</span>
       </div>
       <div className="right-t">
-        <span>Right</span>
+        <div className="Border"> </div>
+        <div className="back"></div>
+
+       <img src={testimonialsData[selected].image} alt="" />
+       <div className="arrows">
+        <img
+        onClick={()=>{
+          selected===0
+          ?setSelected(tLength -1)
+          :setSelected((prev) =>prev -1);
+        }} 
+        src={leftArrow} alt=""  />
+        <img 
+        onClick={()=>{
+          selected===tLength -1
+          ?setSelected(0)
+          :setSelected((prev) =>prev +1);
+        }} src={rightArrow} alt="" />
+       </div>
       </div>
     </div>
   );
